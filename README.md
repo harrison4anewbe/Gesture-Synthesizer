9c403b71d33d412a39badef7d08f3ecdd5a88818
Hongxiang Zhang <hongxiangzhang@campus-105-016.ucdavis.edu>
2023-06-07 17:14:18 -0700

    Combine nlp model and update readme

--- README.md
+++ README.md
@@ -1,6 +1,35 @@
 # Gesture-Synthesizer
-Generating gestures based on input text
+This project aims to create a simple gesture synthesizer that generates gestures based on input text.
+
+We design our workflow into two parts.
+1. Input text process: We utilize large-language-pretrained-model to process the input text, understanding the inner emotions. Specifically, the system will classify the user input into several emotion categories for further processing.
+2. Gesture Generation: We used Maya to create a gesture model. And the system will generate the appropriate gesture video based on the input text and display it on the screen.
+
+Package requirements are lists in requirements.txt
+You can train the model by running the bert_train.py or skip training and generate gestures by running the gesture-synthesizer.py
+
+**Tips: No matter which code you want to run, you need to download the pre-trained model and place it to path `./models/bert_`**
 
 ## Get the models
-Download the pre-trained model and place it to path ./models/bert_  https://drive.google.com/drive/folders/1J_yLCYZ9gjUnQrjwEHK_hv6_M59E-Pfj?usp=sharing
-## Run the code
+Download the pre-trained model and place it to path `./models/bert_`
+https://drive.google.com/drive/folders/1J_yLCYZ9gjUnQrjwEHK_hv6_M59E-Pfj?usp=sharing
+
+## Train the model
+Go the the project root directory and run the following command
+```python3 ./bert_train.py```
+
+The model will be saved to `./models/bert_`
+
+## Test the model
+Go the the project root directory and run the following command
+```python3 ./bert_test.py```
+
+You can change the input text in the code to test the model.
+
+## Generate gestures
+Go the the project root directory and run the following command
+```python3 ./gesture-synthesizer.py```
+
+You can change the input text in the input-box to generate different gestures.
+
+**Tips: Considering different performance of your computer, the model may load from 10 seconds to minutes. If there is no responding when you click the buttom, please wait one minute for model loading.**
---
+++ gesture-synthesizer.py
@@ -0,0 +1,152 @@
+import threading
+import cv2
+import tkinter as tk
+from tkinter import messagebox
+from PIL import Image, ImageTk
+import ktrain
+
+
+# Tooltip class for displaying hints
+class ToolTip(object):
+    def __init__(self, widget):
+        self.widget = widget
+        self.tip_window = None
+        self.id = None
+        self.x = self.y = 0
+        self.text = "Enter an emotion (e.g., Joy, Sad, Anger, Fear, Neutral)"
+
+    def showtip(self):
+        "Display text in tooltip window"
+        x, y, _, _ = self.widget.bbox("insert")
+        x += self.widget.winfo_rootx() + 25
+        y += self.widget.winfo_rooty() + self.widget.winfo_height() + 5
+        # Creates a toplevel window
+        self.tip_window = tw = tk.Toplevel(self.widget)
+        # Leaves only the label and removes the app window
+        tw.wm_overrideredirect(True)
+        tw.wm_geometry(f"+{x}+{y}")
+        label = tk.Label(
+            tw, text=self.text, background="#ffffe0", relief="solid", borderwidth=1
+        )
+        label.pack(ipadx=1)
+
+    def hidetip(self):
+        tw = self.tip_window
+        self.tip_window = None
+        if tw:
+            tw.destroy()
+
+
+def create_tooltip(widget, text):
+    tool_tip = ToolTip(widget)
+
+    def enter(event):
+        tool_tip.showtip()
+
+    def leave(event):
+        tool_tip.hidetip()
+
+    widget.bind("<Enter>", enter)
+    widget.bind("<Leave>", leave)
+
+
+def stop_thread(thread):
+    if thread:
+        for t in threading.enumerate():
+            if t is thread:
+                t._stop()
+
+
+def play_video(video_path, frame, video_label, stop_event):
+    def video_loop():
+        cap = cv2.VideoCapture(video_path)
+        try:
+            while not stop_event.is_set():
+                ret, frame = cap.read()
+                if ret:
+                    image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                    image = ImageTk.PhotoImage(image=Image.fromarray(image))
+                    video_label.config(image=image)
+                    video_label.image = image
+                else:
+                    break
+        finally:
+            cap.release()
+
+    video_label.config(image=None)
+    video_label.image = None
+    thread = threading.Thread(target=video_loop)
+    thread.start()
+    return thread, stop_event
+
+
+def gesture_synthesizer(input_word, frame, video_label, current_thread=None):
+    video_paths = {
+        "joy": "./mov/joy.mov",
+        "sadness": "./mov/sadness.mov",
+        "anger": "./mov/anger.mov",
+        "fear": "./mov/fear.mov",
+        "neutral": "./mov/neutral.mov",
+    }
+    video_path = video_paths.get(input_word.lower())
+    if video_path:
+        messagebox.showinfo(
+            "Gesture synthesizer", f"Generating {input_word} gesture..."
+        )
+        stop_event = threading.Event()
+        if current_thread[0] is not None:  # Check if a thread exists
+            current_thread[1].set()  # stop the previous thread
+            current_thread[0].join()  # wait for the previous thread to finish
+        current_thread = (
+            play_video(video_path, frame, video_label, stop_event),
+            stop_event,
+        )
+
+    return current_thread
+
+
+def generate_gesture(input_word, frame, video_label, predictor, current_thread=None):
+    emotions = ["joy", "sadness", "anger", "fear", "neutral"]
+    message = input_word.lower()
+    prediction = predictor.predict(message)
+    print(prediction)
+    if prediction in emotions:
+        gesture_synthesizer(prediction, frame, video_label, current_thread)
+    else:
+        messagebox.showinfo("Error", "Invalid input. Please enter a valid emotion.")
+    return current_thread
+
+
+def main():
+    window = tk.Tk()
+    window.title("Gesture Synthesizer")
+    window.geometry("800x600")
+
+    predictor = ktrain.load_predictor("./models/bert_")
+
+    frame = tk.Frame(window)
+    frame.place(relx=0.5, rely=0.5, anchor="center")
+
+    video_label = tk.Label(frame)
+    video_label.pack()
+
+    current_thread = None, None
+
+    input_field = tk.Entry(frame)
+    input_field.pack()
+    create_tooltip(input_field, "Enter your text. e.g., How are you?")
+
+    input_button = tk.Button(
+        frame,
+        text="Generate Gesture",
+        command=lambda: generate_gesture(
+            input_field.get(), frame, video_label, predictor, current_thread
+        ),
+    )
+    input_button.pack()
+
+    window.mainloop()
+
+
+if __name__ == "__main__":
+    main()
